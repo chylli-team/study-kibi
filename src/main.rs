4e3ea5a15a7f47cf66d0906f9032f865208d53a2
chylli-binary <chylli@binary.com>
2021-04-05 23:22:18 +0800

    read one character

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::Read;
 fn main() {
-    println!("Hello, world!");
+    let input: Option<i32> = std::io::stdin()
+        .bytes()
+        .next()
+        .and_then(|result| result.ok())
+        .map(|byte| byte as i32);
+        println!("{:?}", input);
 }
